d2043442ec2384921bb6310453b5db69ccd2cb2f
DREYFUS ALOIS <alois.dreyfus@ido-in.com>
2017-08-01 17:04:19 +0200

    libraryTarget: umd

--- config/webpack/webpack-base.js
+++ config/webpack/webpack-base.js
@@ -14,7 +14,7 @@ module.exports = {
   output: {
     filename: '[name].js',
     library: '[name]',
-    libraryTarget: 'window',
+    libraryTarget: 'umd',
     path: outputPath,
     umdNamedDefine: true
   },
